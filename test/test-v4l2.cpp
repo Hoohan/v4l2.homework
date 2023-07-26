37c80828c258f46038aa9ace9ec8d1c52ae160f8
Hoohan <work@hoohan.cn>
2023-07-26 17:02:12 +0800

    add: set format & request frame buffer

--- test/test-v4l2.cpp
+++ test/test-v4l2.cpp
@@ -25,11 +25,11 @@ int main(int argc, char** argv) {
         perror("require capability failed");
         return -1;
     }
-    printf("driver:%s\n",cap.driver);
-    printf("card:%s\n",cap.card);
-    printf("bus_info:%s\n",cap.bus_info);
-    printf("version:%d\n",cap.version);
-    printf("capabilities:%x\n",cap.capabilities);  
+    printf("driver:\t\t%s\n",cap.driver);
+    printf("card:\t\t%s\n",cap.card);
+    printf("bus_info:\t%s\n",cap.bus_info);
+    printf("version:\t%d\n",cap.version);
+    printf("capabilities:\t%x\n",cap.capabilities);  
     if((cap.capabilities & V4L2_CAP_VIDEO_CAPTURE) == V4L2_CAP_VIDEO_CAPTURE)
     {
         printf("Device %s: supports capture.\n",devName.c_str());
@@ -41,8 +41,45 @@ int main(int argc, char** argv) {
 
     //set format
     struct v4l2_format fmt;
-    
+    fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    fmt.fmt.pix.width       = 640;
+    fmt.fmt.pix.height      = 480;
+    fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_RGB24;
+    fmt.fmt.pix.field       = V4L2_FIELD_INTERLACED;
+    ret = v4l2_ioctl(fd, VIDIOC_S_FMT, &fmt);
+    if(ret < 0){
+        perror("set format failed!");
+        return -1;
+    }
+    if (fmt.fmt.pix.pixelformat != V4L2_PIX_FMT_RGB24) {
+                printf("Libv4l didn't accept RGB24 format. Can't proceed.\n");
+                exit(EXIT_FAILURE);
+    }
+    if ((fmt.fmt.pix.width != 640) || (fmt.fmt.pix.height != 480)){
+        printf("Warning: driver is sending image at %dx%d\n", fmt.fmt.pix.width, fmt.fmt.pix.height);
+    }
+    printf("fmt.type:\t\t%d\n",fmt.type);
+    printf("pix.pixelformat:\t%c%c%c%c\n", \
+            fmt.fmt.pix.pixelformat & 0xFF,\
+            (fmt.fmt.pix.pixelformat >> 8) & 0xFF, \
+            (fmt.fmt.pix.pixelformat >> 16) & 0xFF,\
+            (fmt.fmt.pix.pixelformat >> 24) & 0xFF);
+    printf("pix.width:\t\t%d\n",fmt.fmt.pix.width);
+    printf("pix.height:\t\t%d\n",fmt.fmt.pix.height);
+    printf("pix.field:\t\t%d\n",fmt.fmt.pix.field);
 
+    // request frame buf
+    struct v4l2_requestbuffers req;
+    req.count = 4;
+    req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    req.memory = V4L2_MEMORY_MMAP;
+    v4l2_ioctl(fd, VIDIOC_REQBUFS, &req);
+    if(req.count < 2){
+        perror("buffer memory is Insufficient! \n");
+        return -1;
+    }
+
+    // close device
     v4l2_close(fd);
     return 0;
 }
