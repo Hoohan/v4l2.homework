6d6df45ed57e7c491eba3978ba726f5dd2e68adb
Hoohan <work@hoohan.cn>
2023-07-27 12:21:33 +0800

    update: remove v4l-utils

--- README.md
+++ README.md
@@ -2,22 +2,20 @@
 
 使用C++实现从相机读取图片到PC，使用v4l2的函数实现，显示图片在一个窗口上。
 
-## 环境需求(todo)
+## 编译指南(todo)
 
-- Ubuntu / Arch Linux
+### 环境需求
+
+- Ubuntu 20.04 LTS
 - GCC / CMake
-- libv4l2
+- gtkmm-4.0
 - USB Camera
 
-## 使用说明
+### 编译准备
 
-请确保运行环境已安装v4l-utils/gtkmm-4.0
+安装gtkmm-4.0
 
-Arch Linux执行如下命令：
-
-```bash
-sudo pacman -S gtkmm-4.0 libv4l
-```
+### 构建与运行
 
 配置与构建：
 
@@ -30,4 +28,22 @@ cmake --build build
 
 ```bash
 ./bin/grabber
+```
+
+### 常见问题
+
+(todo)
+
+## 参考文档
+
+- [Linux Media Infrastructure userspace API - Part I - Video for Linux API](https://linuxtv.org/downloads/v4l-dvb-apis-new/userspace-api/v4l/v4l2.html)
+- [gtkmm - C++ Interfaces for GTK and GNOME](https://www.gtkmm.org/en/index.html)
+- [Using CMake to build gtkmm programs](https://wiki.gnome.org/Projects/gtkmm/UsingCMake)
+
+## 备注
+
+Arch Linux:
+
+```bash
+sudo pacman -S gtkmm-4.0
 ```
--- test/test-v4l2.cpp
+++ test/test-v4l2.cpp
@@ -1,10 +1,12 @@
 #include <errno.h>
 #include <fcntl.h>
-#include <libv4l2.h>
+#include <unistd.h>
+// #include <libv4l2.h>
 #include <linux/v4l2-controls.h>
 #include <linux/videodev2.h>
 #include <sys/mman.h>
 #include <sys/select.h>
+#include <sys/ioctl.h>
 
 #include <cstddef>
 #include <cstdio>
@@ -22,11 +24,11 @@ struct buffer {
     size_t length;
 };
 
-inline void v4l2_ioctl_safe(int fd, int request, void* arg) {
+static void ioctl_safe(int fd, int request, void* arg) {
     int ret;
 
     do {
-        ret = v4l2_ioctl(fd, request, arg);
+        ret = ioctl(fd, request, arg);
     } while (ret == -1 && ((errno == EINTR) || (errno == EAGAIN)));
 
     if (ret == -1) {
@@ -40,7 +42,7 @@ int main(int argc, char** argv) {
 
     // open the device
     int fd = -1;
-    fd = v4l2_open(devName.c_str(), O_RDWR | O_NONBLOCK, 0);
+    fd = open(devName.c_str(), O_RDWR | O_NONBLOCK, 0);
     if (fd < 0) {
         perror("Can not open device");
     }
@@ -49,7 +51,7 @@ int main(int argc, char** argv) {
 
     // show capability
     struct v4l2_capability cap;
-    v4l2_ioctl_safe(fd, VIDIOC_QUERYCAP, &cap);
+    ioctl_safe(fd, VIDIOC_QUERYCAP, &cap);
 
     printf("driver:\t\t%s\n", cap.driver);
     printf("card:\t\t%s\n", cap.card);
@@ -68,17 +70,11 @@ int main(int argc, char** argv) {
     fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     fmt.fmt.pix.width = 640;
     fmt.fmt.pix.height = 480;
-    fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_RGB24;
+    fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;
+    // fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_RGB24;
     fmt.fmt.pix.field = V4L2_FIELD_INTERLACED;
-    v4l2_ioctl_safe(fd, VIDIOC_S_FMT, &fmt);
-    if (fmt.fmt.pix.pixelformat != V4L2_PIX_FMT_RGB24) {
-        printf("Libv4l didn't accept RGB24 format. Can't proceed.\n");
-        exit(EXIT_FAILURE);
-    }
-    if ((fmt.fmt.pix.width != 640) || (fmt.fmt.pix.height != 480)) {
-        printf("Warning: driver is sending image at %dx%d\n", fmt.fmt.pix.width,
-               fmt.fmt.pix.height);
-    }
+    ioctl_safe(fd, VIDIOC_S_FMT, &fmt);
+
     printf("fmt.type:\t\t%d\n", fmt.type);
     printf("pix.pixelformat:\t%c%c%c%c\n", fmt.fmt.pix.pixelformat & 0xFF,
            (fmt.fmt.pix.pixelformat >> 8) & 0xFF,
@@ -88,12 +84,21 @@ int main(int argc, char** argv) {
     printf("pix.height:\t\t%d\n", fmt.fmt.pix.height);
     printf("pix.field:\t\t%d\n", fmt.fmt.pix.field);
 
+    if (fmt.fmt.pix.pixelformat != V4L2_PIX_FMT_YUYV) {
+        printf("v4l didn't accept YUYV format. Can't proceed.\n");
+        exit(EXIT_FAILURE);
+    }
+    if ((fmt.fmt.pix.width != 640) || (fmt.fmt.pix.height != 480)) {
+        printf("Warning: driver is sending image at %dx%d\n", fmt.fmt.pix.width,
+               fmt.fmt.pix.height);
+    }
+
     // request frame buf
     struct v4l2_requestbuffers req;
     req.count = 4;
     req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     req.memory = V4L2_MEMORY_MMAP;
-    v4l2_ioctl_safe(fd, VIDIOC_REQBUFS, &req);
+    ioctl_safe(fd, VIDIOC_REQBUFS, &req);
     if (req.count < 2) {
         perror("buffer memory is Insufficient! \n");
         return -1;
@@ -109,10 +114,10 @@ int main(int argc, char** argv) {
         buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
         buf.memory = V4L2_MEMORY_MMAP;
         buf.index = bufferNum;
-        v4l2_ioctl_safe(fd, VIDIOC_QUERYBUF, &buf);
+        ioctl_safe(fd, VIDIOC_QUERYBUF, &buf);
         buffers[bufferNum].length = buf.length;
         buffers[bufferNum].start =
-            v4l2_mmap(NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, fd,
+            mmap(NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, fd,
                       buf.m.offset);
 
         if (buffers[bufferNum].start == MAP_FAILED) {
@@ -130,12 +135,12 @@ int main(int argc, char** argv) {
         buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
         buf.memory = V4L2_MEMORY_MMAP;
         buf.index = i;
-        v4l2_ioctl_safe(fd, VIDIOC_QBUF, &buf);
+        ioctl_safe(fd, VIDIOC_QBUF, &buf);
     }
 
     // stream on
     enum v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    v4l2_ioctl_safe(fd, VIDIOC_STREAMON, &type);
+    ioctl_safe(fd, VIDIOC_STREAMON, &type);
 
     // begin capture
     fd_set fds;
@@ -157,29 +162,29 @@ int main(int argc, char** argv) {
         CLEAR(buf);
         buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
         buf.memory = V4L2_MEMORY_MMAP;
-        v4l2_ioctl_safe(fd, VIDIOC_DQBUF, &buf);
+        ioctl_safe(fd, VIDIOC_DQBUF, &buf);
 
         // do some thing
         printf("buf.index: \t%u\n", buf.index);
 
         // q buffer
-        v4l2_ioctl_safe(fd, VIDIOC_QBUF, &buf);
+        ioctl_safe(fd, VIDIOC_QBUF, &buf);
     }
 
     // stream off
     type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    v4l2_ioctl_safe(fd, VIDIOC_STREAMOFF, &type);
+    ioctl_safe(fd, VIDIOC_STREAMOFF, &type);
 
     // unmap buffers
     for (int i = 0; i < bufferNum; i++) {
-        v4l2_munmap(buffers[i].start, buffers[i].length);
+        munmap(buffers[i].start, buffers[i].length);
     }
 
     // free buffers
     free(buffers);
 
     // close device
-    v4l2_close(fd);
+    close(fd);
 
     return 0;
 }
