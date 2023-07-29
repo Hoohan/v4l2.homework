c062c268c5649a6ad2d1d7490ec559b1e641f514
Hoohan <work@hoohan.cn>
2023-07-29 19:23:55 +0800

    downgrade test-gtkmm to gtkmm-3.0

--- test/gtkmm/base.cpp
+++ test/gtkmm/base.cpp
@@ -1,12 +1,15 @@
-#include <gtkmm.h>
+// #include <gtkmm.h>
+
+#include <cairomm/context.h>
+#include <gdkmm/general.h>
+#include <gdkmm/pixbuf.h>
+#include <giomm/resource.h>
+#include <glibmm/fileutils.h>
+#include <gtkmm/application.h>
+#include <gtkmm/drawingarea.h>
+#include <gtkmm/window.h>
 
 #include <iostream>
-
-#include "cairomm/enums.h"
-#include "cairomm/refptr.h"
-#include "gdkmm/pixbuf.h"
-#include "glibmm/refptr.h"
-#include "gtkmm/drawingarea.h"
 
 class MyArea : public Gtk::DrawingArea {
 public:
@@ -14,36 +17,29 @@ public:
     virtual ~MyArea();
 
 protected:
-    void on_draw(const Cairo::RefPtr<Cairo::Context>& cr, int width,
-                 int height);
+    // Override default signal handler:
+    bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override;
+
     Glib::RefPtr<Gdk::Pixbuf> m_image;
 };
 
-class MyWindow : public Gtk::Window {
-public:
-    MyWindow();
-    virtual ~MyWindow();
-
-protected:
-    MyArea m_area;
-};
-
 int main(int argc, char* argv[]) {
-    auto app = Gtk::Application::create("org.gtkmm.examples.base");
+    auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
 
-    return app->make_window_and_run<MyWindow>(argc, argv);
+    Gtk::Window win;
+    win.set_title("DrawingArea");
+    win.set_default_size(600, 400);
+
+    MyArea area;
+    win.add(area);
+    area.show();
+
+    return app->run(win);
 }
-
-MyWindow::MyWindow() {
-    set_title("Basic application Test");
-    set_default_size(600, 300);
-    set_child(m_area);
-}
-
-MyWindow::~MyWindow() {}
-
 MyArea::MyArea() {
     try {
+        // The fractal image has been created by the XaoS program.
+        // http://xaos.sourceforge.net
         m_image = Gdk::Pixbuf::create_from_file("data/test.png");
     } catch (const Gio::ResourceError& ex) {
         std::cerr << "ResourceError: " << ex.what() << std::endl;
@@ -51,21 +47,26 @@ MyArea::MyArea() {
         std::cerr << "PixbufError: " << ex.what() << std::endl;
     }
 
-    if (m_image) {
-        set_content_width(m_image->get_width() / 2);
-        set_content_height(m_image->get_height() / 2);
-    }
-
-    set_draw_func(sigc::mem_fun(*this, &MyArea::on_draw));
+    // Show at least a quarter of the image.
+    if (m_image)
+        set_size_request(m_image->get_width() / 2, m_image->get_height() / 2);
 }
 
 MyArea::~MyArea() {}
 
-void MyArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr, int width,
-                     int height) {
-    if (!m_image) return;
+bool MyArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr) {
+    if (!m_image) return false;
+
+    Gtk::Allocation allocation = get_allocation();
+    const int width = allocation.get_width();
+    const int height = allocation.get_height();
+
+    // Draw the image in the middle of the drawing area, or (if the image is
+    // larger than the drawing area) draw the middle part of the image.
     Gdk::Cairo::set_source_pixbuf(cr, m_image,
                                   (width - m_image->get_width()) / 2,
                                   (height - m_image->get_height()) / 2);
     cr->paint();
+
+    return true;
 }
